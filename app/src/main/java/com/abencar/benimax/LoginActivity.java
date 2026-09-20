46b470155a8644b6e05e600ddcb01e7359d44802
Álvaro Benítez <benigymm@gmail.com>
2026-09-20 20:39:24 +0200

    fixeado el login

--- app/src/main/java/com/abencar/benimax/LoginActivity.java
+++ app/src/main/java/com/abencar/benimax/LoginActivity.java
@@ -78,7 +78,7 @@ public class LoginActivity extends AppCompatActivity {
                         Toast.makeText(LoginActivity.this, "¡Usuario logeado en BeniMax!", Toast.LENGTH_SHORT).show();
                         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                         startActivity(intent);
-
+                        finish();
                     }else{
                         Toast.makeText(LoginActivity.this,"Prueba a registrarte primeto!!", Toast.LENGTH_SHORT).show();
                     }
